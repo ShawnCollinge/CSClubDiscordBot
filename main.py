3429002df342677c260a87fb397dc83238813408
Shawn Collinge <shawncollinge@gmail.com>
2024-03-26 21:29:44 +0000

    added scheduled messages

--- main.py
+++ main.py
@@ -282,6 +282,7 @@ async def list_schedules(ctx):
     
     jobs = scheduler.get_jobs()
     if jobs:
+        print(dir(job))
         response = "**Scheduled Messages:**\n"
         for job in jobs:
             response += f"ID: `{job.id}` - Next Run: {job.next_run_time}\n"
